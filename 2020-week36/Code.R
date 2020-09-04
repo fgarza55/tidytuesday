0ddd5f9c9e74af64fac2aef24579c7be11835024
fgarzafrech <francisco.garza.frech@gmail.com>
2020-09-03 22:25:26 -0500

    First Commit. Tidy data and simple plot

---
+++ 2020-week36/Code.R
@@ -0,0 +1,28 @@
+
+###Tidytuesday 2020 week 36 ####
+
+
+------------------------------------------------------------
+# Get the Data ####
+        
+# Read in with tidytuesdayR package 
+# Install from CRAN via: install.packages("tidytuesdayR")
+#install.packages("tidytuesdayR")
+# This loads the readme and all the datasets for the week of interest
+        
+# Either ISO-8601 date or year/week works!
+        
+        tuesdata <- tidytuesdayR::tt_load(2020, week = 36)
+        
+        key_crop_yields <- tuesdata$key_crop_yields
+
+------------------------------------------------------------
+#Mexico Data
+#Filter
+        
+        
+        
+        
+                
+                
+                
